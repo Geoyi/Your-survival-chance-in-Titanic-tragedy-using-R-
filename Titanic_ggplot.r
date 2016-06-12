d66a957463d60bdde2bc606fd3f0f7d2cb354232
Zhuang-Fang Yi <geospatialanalystyi@gmail.com>
2016-06-11 19:52:57 -0500

    Update Titanic_ggplot.r

--- Titanic_ggplot.r
+++ Titanic_ggplot.r
@@ -49,19 +49,19 @@ titanic <-titanic[1:(n-1),]
 # Check out the structure of titanic
 str(titanic)
 
-# Use ggplot() for the first instruction
+
 ggplot(titanic, aes(x = factor(pclass), fill = factor(sex))) + 
   geom_bar(position = "dodge")
 
-# Use ggplot() for the second instruction
+
 ggplot(titanic, aes(x = factor(pclass), fill = factor(sex))) + 
   geom_bar(position = "dodge") + 
   facet_grid(. ~ survived)
 
-# Position jitter (use below)
+
 posn.j <- position_jitter(0.5, 0)
 
-# Use ggplot() for the last instruction
+
 ggplot(titanic, aes(x = factor(pclass), y = age, col = factor(sex))) + 
   geom_jitter(size = 3, alpha = 0.5, position = posn.j) + 
   facet_grid(. ~ survived)
